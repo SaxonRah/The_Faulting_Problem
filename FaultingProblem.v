9a3786a1245c91be19410231dc73e7c749e04f01
Robert Valentine <paraboliclabs@gmail.com>
2024-05-27 23:38:08 -0400

    Add theorem and proof for conjecture.

--- FaultingProblem.v
+++ FaultingProblem.v
@@ -1,13 +1,25 @@
-Parameter Software : Type. (* Define the type of software *)
+From Coq Require Import Classical.
 
-Parameter decides_bug : Software -> Prop. (* Predicate for deciding if software has bugs *)
+(* Define the type of software *)
+Parameter Software : Type.
 
+(* Define the type of inputs *)
+Parameter Input : Type.
+
+(* Predicate representing the presence of bugs *)
+Parameter B : Software -> Input -> Prop.
+
+(* Predicate for deciding if software has bugs *)
+Parameter decides_bug : Software -> Prop.
+
+(* Bug existence problem definitions *)
 Definition bug_existence_problem : Prop :=
   (exists s, decides_bug s) -> False.
 
 Definition bug_existence_problem' : Prop :=
   forall s, decides_bug s -> False.
 
+(* Equivalence proof between two formulations of the bug existence problem *)
 Theorem statements_equivalent_bug_existence :
   bug_existence_problem <-> bug_existence_problem'.
 Proof.
@@ -15,4 +27,25 @@ Proof.
   - exact (H (ex_intro decides_bug s H0)).
   - destruct H0.
     exact (H x H0).
+Qed.
+
+(* Definition of a bug-free software system *)
+Definition bug_free (s : Software) : Prop :=
+  forall i, ~ B s i.
+
+(* Main theorem stating that if a software system is not bug-free, there exists an input that reveals a bug *)
+Theorem bug_existence_theorem :
+  forall s : Software,
+    ~ bug_free s -> exists i : Input, B s i.
+Proof.
+  intros s H.
+  unfold bug_free in H.
+  apply NNPP.
+  intro contra.
+  apply H.
+  intros i.
+  intro Hbug.
+  apply contra.
+  exists i.
+  exact Hbug.
 Qed.
